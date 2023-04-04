110320c8456ac43116dd3a3275613542b6c37870
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2023-04-04 12:11:20 +0330

    remove items

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 // Varriables
-const courses = document.querySelector('#courses-list');
+const courses = document.querySelector('#courses-list'),
+      shoppingCartContent = document.querySelector('#cart-content tbody');
+      clearCartBtn = document.querySelector('#clear-cart');
 
 
 
@@ -9,6 +11,10 @@ loadEventListeners();
 function loadEventListeners() {
       //when a new course is added
       courses.addEventListener('click', buyCourse);
+
+      shoppingCartContent.addEventListener('click', removeCourse);
+
+      clearCartBtn.addEventListener('click', clearCart);
 }
 
 
@@ -29,5 +35,41 @@ function buyCourse(e) {
 }
 //read the html information of the selected course
 function getCourseInfo(course) {
-      console.log(course);
+      const courseInfo = {
+            image: course.querySelector('img').src,
+            title: course.querySelector('h4').textContent,
+            price: course.querySelector('.price span').textContent,
+            id: course.querySelector('a').getAttribute('data-id')
+      }
+      //insert into the shopping cart
+      addIntoCart(courseInfo);
+}
+function addIntoCart(course) {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+      <tr>
+            <td>
+                  <img src="${course.image}" width=100>
+            </td>
+            <td>${course.title}</td>
+            <td>${course.price}</td>
+            <td>
+                  <a href="#" class="remove" data-id="${course.id}">X</a>
+            </td>
+      </tr>
+      `;
+      shoppingCartContent.appendChild(row);
+}
+function removeCourse(e) {
+      if (e.target.classList.contains('remove')){
+            e.target.parentElement.parentElement.remove();
+      }
+}
+
+function clearCart() {
+      // shoppingCartContent.innerHTML = '';
+
+      while(shoppingCartContent.firstChild) {
+            shoppingCartContent.removeChild(shoppingCartContent.firstChild);
+      }
 }
